18a2ece8d6ef56d65ae6992b0ad30086ceff8187
MatAff <mathijsaffourtit@gmail.com>
2021-08-12 08:33:33 -0600

    simplified using resource based initialization

--- aws_sdk_vpc_ec2.py
+++ aws_sdk_vpc_ec2.py
@@ -1,19 +1,16 @@
 
-"""Document Common AWS SDK Operations
+"""VPC EC2 Webserver
 
-This script is used to document common AWS operations, 
-as well as provide a working example of setting up an
-environment.
-
-Initial goal, spin up vpc, with public subnet, ec2 hosing a small site
+Document to process to set up a VPC, EC2 based webserver.
 """
 
-# https://gist.github.com/nguyendv/8cfd92fc8ed32ebb78e366f44c2daea6
+# Reference material: https://gist.github.com/nguyendv/8cfd92fc8ed32ebb78e366f44c2daea6
 
 import boto3
 import pandas as pd
 
-pd.options.display.max_colwidth = 200
+ec2_client = boto3.client('ec2')
+ec2_res = boto3.resource('ec2')
 
 # --- SETTINGS ---
 
@@ -21,30 +18,16 @@ purpose = 'aws_exploration'
 
 # --- VPC ---
 
-ec2_client = boto3.client('ec2')
-ec2_res = boto3.resource('ec2')
-
 # list vpc
 vpcs = ec2_client.describe_vpcs()
 vpcs_df = pd.DataFrame(vpcs['Vpcs'])
 vpcs_df
 
 # create vpc
-vpc_response = ec2_client.create_vpc(
+vpc = ec2_res.create_vpc(
     CidrBlock='10.0.0.0/24', 
     TagSpecifications=[{'ResourceType': 'vpc', 'Tags': [{'Key': 'purpose', 'Value': purpose}]}]
 )
-vpc_response
-
-# vpc id
-vpc_id = vpc_response['Vpc']['VpcId']
-print(vpc_id)
-
-# instanciate by id
-vpc = ec2_res.Vpc(vpc_id)
-
-# # delete vpc
-# ec2_client.delete_vpc(VpcId=vpc_id) # delete vpc
 
 # --- IGW ---
 
@@ -53,22 +36,9 @@ igws = ec2_client.describe_internet_gateways()
 igws_df = pd.DataFrame(igws['InternetGateways'])
 igws_df
 
-# create internet gateway
-igw_response = ec2_client.create_internet_gateway(
-    TagSpecifications=[{'ResourceType': 'internet-gateway', 'Tags': [{'Key': 'purpose', 'Value': purpose}]}]
-)
-igw_id = igw_response['InternetGateway']['InternetGatewayId']
-print(igw_id)
-
-# instanciate by id
-igw = ec2_res.InternetGateway(igw_id)
-
-# attach to vpc
-igw.attach_to_vpc(VpcId=vpc_id)
-
-# # detach and delete igw
-# vpc.detach_internet_gateway(InternetGatewayId=igw_id)
-# ec2_client.delete_internet_gateway(InternetGatewayId=igw_id)
+# create internet gateway and attach to vpc
+igw = ec2_res.create_internet_gateway(TagSpecifications=[{'ResourceType': 'internet-gateway', 'Tags': [{'Key': 'purpose', 'Value': purpose}]}])
+igw.attach_to_vpc(VpcId=vpc.id)
 
 # --- ROUTE TABLE ---
 
@@ -81,11 +51,9 @@ route_table_df
 route_table = vpc.create_route_table()
 route = route_table.create_route(
     DestinationCidrBlock='0.0.0.0/0',
-    GatewayId=igw_id,
-    TagSpecifications=[{'ResourceType': 'route-table', 'Tags': [{'Key': 'purpose', 'Value': purpose}]}]
+    GatewayId=igw.id,
+    # TagSpecifications=[{'ResourceType': 'route-table', 'Tags': [{'Key': 'purpose', 'Value': purpose}]}]
 )
-route_table_id = route_table.id
-print(route_table_id)
 
 # --- SUBNET ---
 
@@ -95,27 +63,19 @@ subnet_df = pd.DataFrame(subnets['Subnets'])
 subnet_df
 
 # vpc subnets
-vpc_subnets_df = subnet_df[subnet_df.VpcId==vpc_id]
+vpc_subnets_df = subnet_df[subnet_df.VpcId==vpc.id]
 vpc_subnets_df
 
 # create public subnet
-subnet_response = ec2_client.create_subnet(
-    VpcId=vpc_id,
+subnet = ec2_res.create_subnet(
+    VpcId=vpc.id,
     CidrBlock='10.0.0.0/28',
     AvailabilityZone='us-east-1a',
     TagSpecifications=[{'ResourceType': 'subnet', 'Tags': [{'Key': 'purpose', 'Value': purpose}]}]
 )
-subnet_id = subnet_response['Subnet']['SubnetId']
-print(subnet_id)
-
-# instanciate by id
-subnet = ec2_res.InternetGateway(subnet_id)
 
 # associate the route table with the subnet
-route_table.associate_with_subnet(SubnetId=subnet_id)
-
-# # delete
-# ec2_client.delete_subnet(SubnetId=subnet_id)
+route_table.associate_with_subnet(SubnetId=subnet.id)
 
 # --- SECURITY GROUP ---
 
@@ -190,7 +150,7 @@ instances = ec2_res.create_instances(
     MaxCount=1, 
     MinCount=1,
     NetworkInterfaces=[{
-        'SubnetId': subnet_id, 
+        'SubnetId': subnet.id, 
         'DeviceIndex': 0, 
         'AssociatePublicIpAddress': True, 
         'Groups': [sg.group_id]
@@ -199,22 +159,11 @@ instances = ec2_res.create_instances(
 instances[0].wait_until_running()
 print(instances[0].id)
 
-# instances = ec2_res.create_instances(
-#     ImageId=
-#     InstanceType='t2.micro',
-#     KeyName=key_name,
-#     SecurityGroups=(sg_name, ),
-#     UserData=user_data,
-#     IamInstanceProfile={'Name': 'ec2_instance_profile'},
-#     MinCount=1, 
-#     MaxCount=1
-# )
-# instances
-
 # --- CLEAN UP ---
 
 # delete ec2
 ec2_client.terminate_instances(InstanceIds=(instances[0].id,))
+instances[0].wait_until_terminated()
 
 # delete key
 ec2_client.delete_key_pair(KeyName=key_name)
@@ -223,7 +172,7 @@ ec2_client.delete_key_pair(KeyName=key_name)
 ec2_client.delete_security_group(GroupId=sg.id)
 
 # delete subnet
-ec2_client.delete_subnet(SubnetId=subnet_id)
+ec2_client.delete_subnet(SubnetId=subnet.id)
 
 # # delete vpc associated subnets
 # for subnet_id in vpc_subnets_df.SubnetId.values:
@@ -231,11 +180,13 @@ ec2_client.delete_subnet(SubnetId=subnet_id)
 #     ec2_client.delete_subnet(SubnetId=subnet_id)
 
 # delete route table
-ec2_client.delete_route_table(RouteTableId=route_table_id)
+ec2_client.delete_route_table(RouteTableId=route_table.id)
 
 # detach and delete igw
-vpc.detach_internet_gateway(InternetGatewayId=igw_id)
-ec2_client.delete_internet_gateway(InternetGatewayId=igw_id)
+vpc.detach_internet_gateway(InternetGatewayId=igw.id)
+ec2_client.delete_internet_gateway(InternetGatewayId=igw.id)
 
 # delete vpc
-ec2_client.delete_vpc(VpcId=vpc_id) # delete vpc
+ec2_client.delete_vpc(VpcId=vpc.id) # delete vpc
+
+print('finished')
